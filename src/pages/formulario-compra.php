cb34cbca17b482cb8a2cd48e88aaf3632c700ddd
MariDev <mari.d3v@gmail.com>
2024-12-08 14:20:52 +0100

    Validation of data from order form JS

--- src/pages/formulario-compra.php
+++ src/pages/formulario-compra.php
@@ -36,45 +36,54 @@ include("../server/formCarrito.php");
             </div>
         </nav>
         <h1 id="tituloFormulario">Datos de Compra</h1>
-        <form method="POST" action="compra-realizada.php">
+        <form method="POST" name="formulario" action="compra-realizada.php">
             <div id="formulariocontainer">
                 <div>
                     <label>Nombre</label>
-                    <input type="text" name="order_name" placeholder=" Nombre" value="" required>
+                    <input type="text" name="order_name" placeholder=" Nombre" value="">
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Apellidos</label>
-                    <input type="text" name="order_surname"  placeholder=" Apellidos" value="" required>
+                    <input type="text" name="order_surname"  placeholder=" Apellidos" value="">
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Correo electrónico</label>
-                    <input text="email" name="order_email" placeholder=" Correo electrónico" value="" required>
+                    <input type="text" name="order_email" placeholder=" Correo electrónico" value="">
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Teléfono</label>
-                    <input type="tel" name="order_phone" placeholder=" Teléfono" value="" required>
+                    <input type="text" name="order_phone" placeholder=" Teléfono" value="">
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Dirección</label>
-                    <input type="text" name="order_direction" placeholder=" Dirección" value="" required/>
+                    <input type="text" name="order_direction" placeholder=" Dirección" value=""/>
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Datos adicionales</label>
-                    <input type="text" name="order_direction_adicional" placeholder=" Datos adicionales (piso,puerta,barrio...)" value="" required/>
+                    <input type="text" name="order_direction_adicional" placeholder=" Datos adicionales (piso,puerta,barrio...)" value=""/>
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Código postal</label>
-                    <input type="text" name="order_postal_code" placeholder=" Código postal" value="" required/>
+                    <input type="text" name="order_postal_code" placeholder=" Código postal" value=""/>
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Población</label>
-                    <input type="text" name="order_town" placeholder=" Población" value="" required/>
+                    <input type="text" name="order_town" placeholder=" Población" value=""/>
+                    <p class="pError"></p>
                 </div>
                 <div>
                     <label>Provincia</label>
-                    <input type="text" name="order_city" placeholder=" Provincia" value="" required/>
+                    <input type="text" name="order_city" placeholder=" Provincia" value=""/>
+                    <p class="pError"></p>
                 </div>
-                <div style="display:flex;">
+                <div style="display:flex; margin:5px">
                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0,0,256,256"><g fill="#575757" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(10.66667,10.66667)"><path d="M12,2c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c5.511,0 10,-4.489 10,-10c0,-5.511 -4.489,-10 -10,-10zM12,4c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8zM11,7v2h2v-2zM11,11v6h2v-6z"></path></g></g></svg>
                     <p style="font-size:12px; margin:0 4px">No se admiten cambios ni devoluciones.</p>
                 </div>
@@ -110,5 +119,6 @@ include("../server/formCarrito.php");
             </a>
         </div>
     </footer>
+    <script src="../scripts/formBuyValidation.js"></script>
 </body>
 </html>
---
+++ src/scripts/formBuyValidation.js
@@ -0,0 +1,245 @@
+let formulario = document.getElementsByName("formulario")[0];
+
+let nombre = document.getElementsByName("order_name")[0];
+let apellidos = document.getElementsByName("order_surname")[0];
+let email = document.getElementsByName("order_email")[0];
+let telefono = document.getElementsByName("order_phone")[0];
+let direccion = document.getElementsByName("order_direction")[0];
+let direccion_adi = document.getElementsByName("order_direction_adicional")[0];
+let codigo_postal = document.getElementsByName("order_postal_code")[0];
+let poblacion = document.getElementsByName("order_town")[0];
+let provincia = document.getElementsByName("order_city")[0];
+
+let boton = document.getElementsByName("docompradefinitiva")[0];
+
+let parrafos = document.getElementsByClassName("pError");
+
+window.onload = function(){ 
+    let expresionNomApe = /^[a-zA-ZÀ-ÿ\s]{2,50}$/;
+
+    nombre.addEventListener("change", function(){
+        if (nombre.value == ""){
+            nombre.style.border = "1px solid red";
+            parrafos[0].innerHTML = "Este campo no puede estar vacío";
+        } else if(!expresionNomApe.test(nombre.value)){
+            nombre.style.border = "1px solid red";
+            parrafos[0].innerHTML = "El nombre introducido no es válido";
+        } else if (expresionNomApe.test(nombre.value)){
+            nombre.style.border = "none";
+            parrafos[0].innerHTML = "";
+        }
+    })
+
+    apellidos.addEventListener("change", function(){
+        if (apellidos.value == ""){
+            apellidos.style.border = "1px solid red";
+            parrafos[1].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionNomApe.test(apellidos.value)){
+            apellidos.style.border = "1px solid red";
+            parrafos[1].innerHTML = "Los apellidos introducidos no son válidos";
+        } else if (expresionNomApe.test(apellidos.value)){
+            apellidos.style.border = "none";
+            parrafos[1].innerHTML = "";
+        }
+    })
+
+    let expresionEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    email.addEventListener("change", function(){
+
+        if (email.value == ""){
+            email.style.border = "1px solid red";
+            parrafos[2].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionEmail.test(email.value)){
+            email.style.border = "1px solid red";
+            parrafos[2].innerHTML = "El email introducido no es válido";
+        } else if (expresionEmail.test(email.value)){
+            email.style.border = "none";
+            parrafos[2].innerHTML = "";
+        }
+    })
+
+    let expresionTel = /^\+?\d{1,4}[-.\s]?\d{1,4}(\s?\d{2,4}){2,7}$/
+    
+    telefono.addEventListener("change", function(){
+
+        if (telefono.value == ""){
+            telefono.style.border = "1px solid red";
+            parrafos[3].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionTel.test(telefono.value)){
+            telefono.style.border = "1px solid red";
+            parrafos[3].innerHTML = "El telefono introducido no es válido";
+        } else if (expresionTel.test(telefono.value)){
+            telefono.style.border = "none";
+            parrafos[3].innerHTML = "";
+        }
+    })
+
+    let expresionDirec = /^[a-zA-Z0-9À-ÿ\s,.\-\/]{2,100}$/;
+
+    direccion.addEventListener("change", function(){
+
+        if (direccion.value == ""){
+            direccion.style.border = "1px solid red";
+            parrafos[4].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionDirec.test(direccion.value)){
+            direccion.style.border = "1px solid red";
+            parrafos[4].innerHTML = "La dirección introducida no es válida";
+        } else  if (expresionDirec.test(direccion.value)){
+            direccion.style.border = "none";
+            parrafos[4].innerHTML = "";
+        }
+    })
+
+    direccion_adi.addEventListener("change", function(){
+
+        if (direccion_adi.value == ""){
+            direccion_adi.style.border = "1px solid red";
+            parrafos[5].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionDirec.test(direccion_adi.value)){
+            direccion_adi.style.border = "1px solid red";
+            parrafos[5].innerHTML = "La dirección introducida no es válida";
+        } else  if (expresionDirec.test(direccion_adi.value)){
+            direccion_adi.style.border = "none";
+            parrafos[5].innerHTML = "";
+        }
+    })
+    
+    let expresionCodigo = /^\d{4,10}$/;
+
+    codigo_postal.addEventListener("change", function(){
+
+        if (codigo_postal.value == ""){
+            codigo_postal.style.border = "1px solid red";
+            parrafos[6].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionCodigo.test(codigo_postal.value)){
+            codigo_postal.style.border = "1px solid red";
+            parrafos[6].innerHTML = "El código postal introducido no es válido";
+        } else if (expresionCodigo.test(codigo_postal.value)){
+            codigo_postal.style.border = "none";
+            parrafos[6].innerHTML = "";
+        }
+    })
+
+    let expresionPoPro = /^[a-zA-ZÀ-ÿ\s]{2,50}$/;
+
+    poblacion.addEventListener("change", function(){
+
+        if (poblacion.value == ""){
+            poblacion.style.border = "1px solid red";
+            parrafos[7].innerHTML = "Este campo no puede estar vacío";
+        } else if (!expresionPoPro.test(poblacion.value)){
+            poblacion.style.border = "1px solid red";
+            parrafos[7].innerHTML = "La población introducida no es válida";
+        } else if (expresionPoPro.test(poblacion.value)) {
+            poblacion.style.border = "none";
+            parrafos[7].innerHTML = "";
+        }
+    })
+
+    provincia.addEventListener("change", function(){
+
+        if (provincia.value == ""){
+            provincia.style.border = "1px solid red";
+            parrafos[8].innerHTML = "Este campo no puede estar vacío";
+        }  else if (!expresionPoPro.test(provincia.value)){
+            provincia.style.border = "1px solid red";
+            parrafos[8].innerHTML = "La provincia introducida no es válida";
+        } else if (expresionPoPro.test(provincia.value)) {
+            provincia.style.border = "none";
+            parrafos[8].innerHTML = "";
+        }
+    })
+
+    formulario.addEventListener("submit",function(event){
+        if(nombre.value == ""){
+            event.preventDefault();
+            parrafos[0].innerHTML= "Este campo no puede estar vacío";
+            nombre.style.border= "1px solid red";
+        } else if (!expresionNomApe.test(nombre.value)){
+            event.preventDefault();
+            nombre.style.border = "1px solid red";
+            parrafos[0].innerHTML = "El nombre introducido no es válido";
+        }
+        
+        if (apellidos.value == ""){
+            event.preventDefault();
+            parrafos[1].innerHTML= "Este campo no puede estar vacío";
+            apellidos.style.border = "1px solid red";
+        } else if(!expresionNomApe.test(apellidos.value)){
+            event.preventDefault();
+            apellidos.style.border = "1px solid red";
+            parrafos[1].innerHTML = "Los apellidos introducidos no son válidos";
+        }
+        
+        if (email.value == ""){
+            event.preventDefault();
+            parrafos[2].innerHTML= "Este campo no puede estar vacío";
+            email.style.border = "1px solid red";
+        } else if(!expresionEmail.test(email.value)){
+            event.preventDefault();
+            email.style.border = "1px solid red";
+            parrafos[2].innerHTML = "El email introducido no es válido";
+        }
+        
+        if (telefono.value == ""){
+            event.preventDefault();
+            parrafos[3].innerHTML= "Este campo no puede estar vacío";
+            telefono.style.border = "1px solid red";
+        } else if (!expresionTel.test(telefono.value)){
+            event.preventDefault();
+            telefono.style.border = "1px solid red";
+            parrafos[3].innerHTML = "El telefono introducido no es válido";
+        }
+        
+        if (direccion.value == ""){
+            event.preventDefault();
+            parrafos[4].innerHTML= "Este campo no puede estar vacío";
+            direccion.style.border = "1px solid red";
+        } else if (!expresionDirec.test(direccion.value)){
+            event.preventDefault();
+            direccion.style.border = "1px solid red";
+            parrafos[4].innerHTML = "La dirección introducida no es válida";
+        }
+        
+        if (direccion_adi.value == ""){
+            event.preventDefault();
+            parrafos[5].innerHTML= "Este campo no puede estar vacío";
+            direccion_adi.style.border = "1px solid red";
+        } else if (!expresionDirec.test(direccion_adi.value)){
+            event.preventDefault();
+            direccion_adi.style.border = "1px solid red";
+            parrafos[5].innerHTML = "La dirección introducida no es válida";
+        }
+        
+        if (codigo_postal.value == ""){
+            event.preventDefault();
+            parrafos[6].innerHTML= "Este campo no puede estar vacío";
+            codigo_postal.style.border = "1px solid red";
+        } else if (!expresionCodigo.test(codigo_postal.value)){
+            event.preventDefault();
+            codigo_postal.style.border = "1px solid red";
+            parrafos[6].innerHTML = "El código postal introducido no es válido";
+        }
+        
+        if (poblacion.value == ""){
+            event.preventDefault();
+            parrafos[7].innerHTML= "Este campo no puede estar vacío";
+            poblacion.style.border = "1px solid red";
+        } else if (!expresionPoPro.test(poblacion.value)){
+            event.preventDefault();
+            poblacion.style.border = "1px solid red";
+            parrafos[7].innerHTML = "La población introducida no es válida";
+        } 
+        
+        if (provincia.value == ""){
+            event.preventDefault();
+            parrafos[8].innerHTML= "Este campo no puede estar vacío";
+            provincia.style.border = "1px solid red";
+        } else if (!expresionPoPro.test(provincia.value)){
+            event.preventDefault();
+            provincia.style.border = "1px solid red";
+            parrafos[8].innerHTML = "La provincia introducida no es válida";
+        }
+    })
+}
